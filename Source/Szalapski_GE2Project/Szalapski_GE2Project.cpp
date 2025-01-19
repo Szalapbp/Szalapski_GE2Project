f68771a62b8ff47b4a09e8d25c35af84e89f2dc5
Szalapbp <szalapbp@mail.uc.edu>
2025-01-18 23:44:11 -0500

    Initial commit

---
+++ Source/Szalapski_GE2Project/Szalapski_GE2Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Szalapski_GE2Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Szalapski_GE2Project, "Szalapski_GE2Project" );
